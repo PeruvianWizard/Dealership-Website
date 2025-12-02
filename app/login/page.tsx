f41137d312b0f211499590419ee9975529809e63
PeruvianWizard <aprilexlpe@gmail.com>
2025-12-01 21:58:03 -0700

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from  './Login.module.css';
 
 export default function Login() {
-    <main>
-        
-    </main>
+    return(
+        <main className={styles.container}>
+            <div className={styles.txt}>
+                <h1>Hi</h1>
+            </div>
+        </main>
+    );
 }
