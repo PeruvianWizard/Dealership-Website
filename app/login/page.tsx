62bd495b16619769fb25a0cf05d861735a4d65ab
PeruvianWizard <aprilexlpe@gmail.com>
2025-12-02 22:47:52 -0700

    Progress

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,6 +12,10 @@ export default function Login() {
         <div className='h-screen w-screen flex justify-center items-center bg-slate-100'>
             <div className='sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12'>
                 <h1 className='font-semibold text-2xl'>Login</h1>
+                <p className='text-center'>
+                    Need to create an account?{' '}
+                    <Link className='text-indigo-500 hover:underline' href={'/'}>Create an account</Link>
+                </p>
             </div>
         </div>
     );
