a163a399d3018327d272644fad900a9da36928bb
PeruvianWizard <aprilexlpe@gmail.com>
2025-12-02 21:56:04 -0700

    tailwind attempt

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,10 +4,15 @@ import styles from  './Login.module.css';
 
 export default function Login() {
     return(
-        <main className={styles.container}>
-            <div className={styles.txt}>
-                <h1>Hi</h1>
+        // <main className={styles.container}>
+        //     <div className={styles.txt}>
+        //         <h1>Hi</h1>
+        //     </div>
+        // </main>
+        <div className='h-screen w-screen flex justify-center items-center bg-slate-100'>
+            <div className='sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12'>
+                <h1 className='font-semibold text-2xl'>Login</h1>
             </div>
-        </main>
+        </div>
     );
 }
