debcaf5557a2eac2e3b40022252a31a82f3689b7
PeruvianWizard <aprilexlpe@gmail.com>
2025-12-02 23:51:22 -0700

    Login page and new styles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from  './Login.module.css';
+import { Form } from '@/app/login/form';
 
 export default function Login() {
     return(
-        // <main className={styles.container}>
-        //     <div className={styles.txt}>
-        //         <h1>Hi</h1>
-        //     </div>
-        // </main>
         <div className='h-screen w-screen flex justify-center items-center bg-slate-100'>
-            <div className='sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12'>
+            <div className='sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-8'>
                 <h1 className='font-semibold text-2xl'>Login</h1>
+                <Form />
                 <p className='text-center'>
                     Need to create an account?{' '}
-                    <Link className='text-indigo-500 hover:underline' href={'/'}>Create an account</Link>
+                    <Link className='text-blue-500 hover:underline' href={'/register'}>Create an account</Link>
                 </p>
             </div>
         </div>
